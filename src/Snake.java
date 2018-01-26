2402166e60dfe59374a4d3157b14f5680e09631f
HosniBerker <Hosni.berker@gmail.com>
2018-01-25 22:39:46 -0500

    adding framework

--- src/Snake.java
+++ src/Snake.java
@@ -1,18 +1,49 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Snake {
     private int NumBlocks;
     private boolean Alive;
+    private int xLocation;
+    private int yLocation;
+    private ArrayList<JPanel> boxes;
 
-    public Snake(int size, boolean living) {
+    public Snake(int xLoc, int yLoc, int size, boolean living) {
         NumBlocks = size;
-
+        Alive = living;
+        yLocation = yLoc;
+        xLocation = xLoc;
     }
+    public void updatLoc(int XChange, int YChange){
+        xLocation += XChange;
+        yLocation += YChange;
+    }
+    public ArrayList<JPanel> getList(){
+        return boxes;
+    }
+    public void updateList(ArrayList<JPanel> newB){
+        boxes = newB;
+    }
+
+
     public void increaseSize(){
         NumBlocks++;
     }
+
     public void dead(){
         Alive = false;
     }
-    public void isAlive(){
+    public int getSize(){
+        return NumBlocks;
+    }
+    public boolean isAlive(){
+        return Alive;
 
+    }
+    public int getY(){
+        return yLocation;
+    }
+    public int getX(){
+        return xLocation;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,16 +1,80 @@
+import javafx.scene.shape.DrawMode;
+import java.awt.Graphics;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 public class main {
+    static final int SIZE = 500;
     static JFrame gameScreen = new JFrame("Snake");
     static JPanel board = new JPanel();
+    static Snake player = new Snake(0,0,1,true);
+
+    private static int xDir = 0;
+    private static int yDir = 1;
 
     public static void initFrame(){
-        gameScreen.setSize(500,500);
+        gameScreen.setSize(SIZE,SIZE);
         gameScreen.setVisible(true);
         gameScreen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+        JPanel view = new JPanel();
+        view.setLayout(null);
+        view.setVisible(true);
+        view.setBackground(Color.BLACK);
+        gameScreen.add(view);
+        gameScreen.revalidate();
+
+        JPanel firstBox = new JPanel();
+        firstBox.setBackground(Color.WHITE);
+        firstBox.setLocation(10,10);
+        firstBox.setSize(10,10);
+
+        view.add(firstBox);
+
+
+        ArrayList<JPanel> box = player.getList();
+        box.add(firstBox);
+        player.updateList(box);
+
+        view.revalidate();
+    }
+    public static void isDead(){
+        if(player.getX() > SIZE || player.getX() < 0 || player.getY() > 500 || player.getY() < 0){
+            player.dead();
+        }
+    }
+
+    public static void initSnke(){
+        for (int i = 0; i < player.getSize(); i++) {
+
+        }
+
+
+    }
+    private static void update(){
+        for (int i = 0; i < player.getList().size(); i++){
+            JPanel x = player.getList().get(i);
+            player.getList().get(i).setLocation(x.getLocation().x + xDir,x.getLocation().y);
+        }
     }
     public static void main(String[] args){
         initFrame();
+        boolean on = true;
+        while(player.isAlive() || on){
+
+
+            isDead();
+            update();
+            try {
+
+            }
+            except
+            TimeUnit.SECONDS.sleep(1);
+            on = false;
+        }
+
 
     }
 
